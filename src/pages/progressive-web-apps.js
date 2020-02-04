907d8171f2e3808f6ed4bf20898a38c5ac4f4f42
Anshul889 <anshulmanaktala@gmail.com>
2020-02-04 13:41:42 +0530

    pwa icons

--- src/pages/progressive-web-apps.js
+++ src/pages/progressive-web-apps.js
@@ -3,9 +3,13 @@ import React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import Image from 'gatsby-image'
 import Layout from '../components/layout'
+import { Spring, config } from 'react-spring/renderprops'
 
-import laptop from '../images/laptop.svg'
 import mobile from '../images/mobile-duotone.svg'
+import check from '../images/check.svg'
+import cross from '../images/cross.svg'
+import playstore from '../images/playstore.svg'
+import chrome from '../images/chrome.svg'
 
 const getImages = graphql`
   {
@@ -44,7 +48,6 @@ const ProgressiveWebApp = () => {
             <h1>PROGRESSIVE WEB APPS</h1>
           </div>
           <div className="contentpage">
-            <img src={mobile} alt="" height={100} />
             <p>
               A high-quality Progressive Web App has incredible benefits, making
               it easy to delight your users, grow engagement and increase
@@ -58,21 +61,34 @@ const ProgressiveWebApp = () => {
               offered) to web applications. The concept of website has become
               obsolete and the term Wep App has replaced it.
             </p>
+            <div className="pwaimg">
+              <div style={{ position: 'relative' }}>
+                <img src={mobile} alt="" height={100} />
+                <img className="pwaicon" src={check} alt="" />
+                <img className="pwaappicon" src={chrome} alt=""/>
+              </div>
+              <div style={{ position: 'relative' }}>
+                <img src={mobile} alt="" height={100} />
+                <img className="pwaicon" src={cross} alt="" />
+                <img className="pwaappicon" src={playstore} alt=""/>
+              </div>
+            </div>
+
             <p>
-              For businesses globally{' '}
+              For businesses globally
               <b>60-70 percent of traffic comes from mobile browsers</b>. Users
               don’t want to download and install a native app unless they feel
               they would use it every day. Globally businesses are transitioning
               from website to web apps which are being created on a daily basis
             </p>
             <p>
-              The term <b>Progressive Web apps</b> is used to describe{' '}
+              The term <b>Progressive Web apps</b> is used to describe
               <b>
                 web apps that use new features supported by modern browsers.
               </b>
             </p>
             <p>
-              At Caricode we build progressive web apps that deliver{' '}
+              At Caricode we build progressive web apps that deliver
               <b>
                 high performance, engaging, native-like experience, and the
                 ability to work offline
@@ -83,7 +99,7 @@ const ProgressiveWebApp = () => {
             <p>
               Transitioning your business from a website to a Progressive Web
               App offers major benefits, most importantly, superior performance
-              and increased conversions. Our Progressive Web Apps are{' '}
+              and increased conversions. Our Progressive Web Apps are
               <b>responsive, fast, and connection-independent.</b>
             </p>
             <p>
@@ -104,15 +120,11 @@ const ProgressiveWebApp = () => {
               </li>
               <li>Access to camera and Microphone</li>
             </ul>
-            <div className="pwaexamplediv">
               <div className="pwaexampleheader">
                 <h4>
                   A number of companies have succeeded with Progressive Web
                   Apps:
                 </h4>
-              </div>
-              <div className="pwaexamples">
-                <div className="pwaexample">
                   <h5>Twitter</h5>
                   <p>
                     The Twitter Lite Progressive Web App became the default
@@ -124,24 +136,19 @@ const ProgressiveWebApp = () => {
                     <li>75% increase in Tweets sent</li>
                     <li>20% decrease in bounce rate</li>
                   </ul>
-                </div>
-                <div className="pwaexample">
+
                   <h5>Flipkart</h5>
                   <p>
                     Flipkart, India’s largest e-commerce site, decided to
                     combine their web presence and native app into a Progressive
                     Web Application, resulting in a 70% increase in conversions.
                   </p>
-                </div>
-                <div className="pwaexample">
                   <h5>Wego</h5>
                   <p>
                     After building a PWA, the travel company Wego noticed a 26%
                     increase in the number of users, a 95% increase in
                     conversions on Android, and a 50% increase on iOS.
                   </p>
-                </div>
-                <div className="pwaexample">
                   <h5>BookMyShow</h5>
                   <p>
                     BookMyShow India’s largest ticketing firm, with 50+ million
@@ -150,8 +157,6 @@ const ProgressiveWebApp = () => {
                     an 80+% increase in conversions, which means more users
                     purchasing tickets.
                   </p>
-                </div>
-              </div>
             </div>
           </div>
           <div className="sidebar">
