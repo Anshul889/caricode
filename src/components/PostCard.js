ceb530de180765f0d5f66abbca29e228d80f341f
Anshul889 <anshulmanaktala@gmail.com>
2020-04-27 20:34:55 +0530

    seo image

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -29,16 +29,16 @@ const PostCard = ({ post }) => {
             <div className={styles.carddate}>{date}</div>
             <div className={styles.cardshare}>
               <div className={styles.cardshareshare}>Share:</div>
-              <FacebookShareButton url={`https://mensfashion.netlify.app/${slug}`} style={{height: '24px'}}>
+              <FacebookShareButton url={`https://www.caricode.co/${slug}/`} style={{height: '24px'}}>
                 <FacebookIcon size={24} round bgStyle={{fill: 'lightgray'}}/>
               </FacebookShareButton>
-              <EmailShareButton url={`https://mensfashion.netlify.app/${slug}`} style={{height: '24px'}}>
+              <EmailShareButton url={`https://www.caricode.co/${slug}/`} style={{height: '24px'}}>
                 <EmailIcon size={24} round bgStyle={{fill: 'lightgray'}}/>
               </EmailShareButton>
-              <WhatsappShareButton url={`https://mensfashion.netlify.app/${slug}`} style={{height: '24px'}}>
+              <WhatsappShareButton url={`https://www.caricode.co/${slug}/`} style={{height: '24px'}}>
                 <WhatsappIcon size={24} round bgStyle={{fill: 'lightgray'}}/>
               </WhatsappShareButton>
-              <TwitterShareButton url={`https://mensfashion.netlify.app/${slug}`} style={{height: '24px'}}>
+              <TwitterShareButton url={`https://www.caricode.co/${slug}/`} style={{height: '24px'}}>
                 <TwitterIcon size={24} round bgStyle={{fill: 'lightgray'}}/>
               </TwitterShareButton>
             </div>
