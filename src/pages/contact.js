3d1f8936ac109e71605b9a2f3aed05b48b2912a2
Anshul889 <anshulmanaktala@gmail.com>
2020-04-28 10:34:01 +0530

    address

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,6 +10,15 @@ const contact = () => {
       <div className={styles.container}>
         <div className={styles.inner}>
           <Contact />
+          <div className={styles.address}>
+            Address: Wework, C - 20, G Block Rd, G Block BKC, Bandra Kurla
+            Complex, Mumbai, Maharashtra 400051
+          </div>
+          <div className={styles.getdirections}>
+            <a href="https://www.google.com/maps/place/Caricode/@19.0587205,72.8629498,17z/data=!3m1!4b1!4m5!3m4!1s0x3be7c991b19ae605:0xe831624218e9e749!8m2!3d19.0587205!4d72.8651385">
+              <button>Get Directions</button>
+            </a>
+          </div>
           <MapContainer />
         </div>
       </div>
