2997009eb428a5eefba7365e405b083c3b42d44e
Anshul889 <anshulmanaktala@gmail.com>
2020-05-06 09:06:37 +0530

    contact animation

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
 import { Spring, config } from 'react-spring/renderprops'
 import VisibilitySensor from 'react-visibility-sensor'
+import { useTransition, animated } from 'react-spring'
 
 const Contact = () => {
   const [message, setMessage] = useState(null)
   const [inputField, setInputfield] = useState(null)
   const [formError, setError] = useState(false)
+  const transitions = useTransition(formError &&
+    !(message && inputField), null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: config.slow,
+  })
   return (
     <section className="contact">
       <h2>SEND US A MESSAGE</h2>
@@ -53,15 +61,14 @@ const Contact = () => {
             Send
           </button>
         )}
-        {formError && !(message && inputField) && (
-          <Spring
-            config={config.slow}
-            from={{ opacity: 0, color: 'red' }}
-            to={{ opacity: 1, color: 'red' }}
-          >
-            {props => <span style={props}> Form is incomplete!</span>}
-          </Spring>
-        )}
+    {transitions.map(
+            ({ item, key, props }) =>
+              item && (
+                <animated.span key={key} style={{...props, color: 'red'}}>
+                  Form is Incomplete
+                </animated.span>
+              )
+          )}
       </form>
     </section>
   )
