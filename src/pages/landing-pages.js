d534e39ab09b69b0ce233680714be0c7e9523cb7
Anshul889 <anshulmanaktala@gmail.com>
2020-07-27 14:17:16 +0530

    all parallax layers

--- src/pages/landing-pages.js
+++ src/pages/landing-pages.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import SEO from '../components/SEO'
 import Layout from '../components/layout'
+import chart from '../images/chart-line.svg'
+import question from '../images/question.svg'
+import user from '../images/user.svg'
 import { useStaticQuery, graphql } from 'gatsby'
 import styles from './landingpage.module.css'
 import { useEffect } from 'react'
@@ -21,31 +24,79 @@ const LandingPages = () => {
         <ParallaxLayer
           offset={1}
           speed={1}
-          style={{ backgroundColor: '#805E73' }}
+          style={{ backgroundColor: '#3F51B5' }}
 
         />
         <ParallaxLayer
           offset={2}
           speed={1}
-          style={{ backgroundColor: '#87BCDE' }}
+          style={{ backgroundColor: '#00897B' }}
         />
          <ParallaxLayer
           offset={3}
           speed={1}
-          style={{ backgroundColor: '#805E73' }}
+          style={{ backgroundColor: '#673AB7' }}
 
         />
         <ParallaxLayer
           offset={4}
           speed={1}
-          style={{ backgroundColor: '#87BCDE' }}
+          style={{ backgroundColor: '#3F51B5' }}
         />
-
         <ParallaxLayer
           offset={0}
           speed={0}
           factor={5}
         />
+
+        {/* svgs and text */} 
+        
+        <ParallaxLayer offset={0} speed={0.5}>
+          <img src={chart} style={{display: 'block', width: '40%', margin: '0 auto', transform: 'translateY(-130px)'}} />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.15} speed={0.3}>
+          <div style={{width: '90%', margin: '0 auto', textAlign: 'center'}}>Boost Conversions - Get more sales with a pro landing page</div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.75} speed={0.3}>
+          <div style={{width: '90%', margin: '0 auto', textAlign: 'center'}}>Your landing page web design has only one purpose: To get people to act, usually with a CTA button or by filling in your opt-in-form.</div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1.0} speed={0.1}  >
+          <img src={question} style={{display: 'block', width: '9%', margin: '0 auto',  transform: 'translateY(-95px)'}} />
+        </ParallaxLayer>
+        <ParallaxLayer offset={1.07} speed={0.1}  >
+          <img src={user} style={{display: 'block', width: '10%', margin: '0 auto',  transform: 'translateY(-100px)'}} />
+        </ParallaxLayer>
+        <ParallaxLayer offset={1.07} speed={0.2}>
+          <div style={{width: '90%', margin: '0 auto', textAlign: 'center'}}>“The paradox of choice reveals that less is more. Too many options will often hinder people from making a choice.”</div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={2.0} speed={0.1}  >
+          <img src={question} style={{display: 'block', width: '9%', margin: '0 auto',  transform: 'translateY(-95px)'}} />
+        </ParallaxLayer>
+        <ParallaxLayer offset={2.07} speed={0.1}  >
+          <img src={user} style={{display: 'block', width: '10%', margin: '0 auto',  transform: 'translateY(-100px)'}} />
+        </ParallaxLayer>
+        <ParallaxLayer offset={2.07} speed={0.2}>
+          <div style={{width: '90%', margin: '0 auto', textAlign: 'center'}}>“The paradox of choice reveals that less is more. Too many options will often hinder people from making a choice.”</div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={3.0} speed={0.1}  >
+          <img src={question} style={{display: 'block', width: '9%', margin: '0 auto',  transform: 'translateY(-95px)'}} />
+        </ParallaxLayer>
+        <ParallaxLayer offset={3.07} speed={0.1}  >
+          <img src={user} style={{display: 'block', width: '10%', margin: '0 auto',  transform: 'translateY(-100px)'}} />
+        </ParallaxLayer>
+        <ParallaxLayer offset={3.07} speed={0.2}>
+          <div style={{width: '90%', margin: '0 auto', textAlign: 'center'}}>“The paradox of choice reveals that less is more. Too many options will often hinder people from making a choice.”</div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={4.0} speed={0.1}  >
+          <img src={question} style={{display: 'block', width: '9%', margin: '0 auto',  transform: 'translateY(-95px)'}} />
+        </ParallaxLayer>
+        <ParallaxLayer offset={4.07} speed={0.1}  >
+          <img src={user} style={{display: 'block', width: '10%', margin: '0 auto',  transform: 'translateY(-100px)'}} />
+        </ParallaxLayer>
+        <ParallaxLayer offset={4.07} speed={0.2}>
+          <div style={{width: '90%', margin: '0 auto', textAlign: 'center'}}>“The paradox of choice reveals that less is more. Too many options will often hinder people from making a choice.”</div>
+        </ParallaxLayer>
+
         </Parallax>
 
         
