bd18778170021d5f0ade0d57eea49a53d715f06c
Anshul889 <anshulmanaktala@gmail.com>
2020-09-20 09:12:11 +0530

    removed section 2 home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Banner from '../components/banner'
-import SectionTwo from '../components/SectionTwo'
 import Contact from '../components/contact'
 import SectionThree from '../components/SectionThree'
 import SectionFour from '../components/SectionFour'
@@ -13,7 +12,6 @@ export default () => (
   <Layout>
     <SEO />
     <Banner />
-    <SectionTwo />
     <SectionThree />
     <SectionFour />
     <Contact />
