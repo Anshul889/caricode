1dc72b54bbbc8caa9b2a5a8d9239abf2c0c8e8e4
Anshul889 <anshulmanaktala@gmail.com>
2020-09-21 09:18:36 +0530

    refactored blogs page

---
+++ src/components/BlogHero.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Spring, config } from 'react-spring/renderprops'
+import styles from '../pages/blogs.module.css'
+import window from '../images/window-white.svg'
+import user from '../images/user.svg'
+
+
+const BlogHero = () => {
+  return (
+    <div className={styles.hero}>
+      <h1>Want more traffic to your website ?</h1>
+      <div className={styles.heroimg}>
+        <Spring
+          from={{ transform: 'translate(-30px, -30px)' }}
+          to={{ transform: 'translate(0, 0)' }}
+          config={config.molasses}
+          delay={800}
+        >
+          {props => <img className={styles.usersvg} src={user} style={props} />}
+        </Spring>
+        <Spring
+          from={{ transform: 'translate(0px, -50px)' }}
+          to={{ transform: 'translate(0, -20px)' }}
+          config={config.molasses}
+          delay={800}
+        >
+          {props => <img className={styles.usersvg} src={user} style={props} />}
+        </Spring>
+        <Spring
+          from={{ transform: 'translate(30px, -30px)' }}
+          to={{ transform: 'translate(0, 0px)' }}
+          config={config.molasses}
+          delay={800}
+        >
+          {props => <img className={styles.usersvg} src={user} style={props} />}
+        </Spring>
+        <img className={styles.windowsvg} src={window} />
+        <Spring
+          from={{ transform: 'translate(-30px, 30px)' }}
+          to={{ transform: 'translate(0, 0px)' }}
+          config={config.molasses}
+          delay={800}
+        >
+          {props => <img className={styles.usersvg} src={user} style={props} />}
+        </Spring>
+        <Spring
+          from={{ transform: 'translate(0px, 50px)' }}
+          to={{ transform: 'translate(0, 20px)' }}
+          config={config.molasses}
+          delay={800}
+        >
+          {props => <img className={styles.usersvg} src={user} style={props} />}
+        </Spring>
+        <Spring
+          from={{ transform: 'translate(30px, 30px)' }}
+          to={{ transform: 'translate(0, 0px)' }}
+          config={config.molasses}
+          delay={800}
+        >
+          {props => <img className={styles.usersvg} src={user} style={props} />}
+        </Spring>
+      </div>
+    </div>
+  )
+}
+
+export default BlogHero
